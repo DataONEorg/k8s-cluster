8edd6a1854be1a7eabbb1b513e439523f95af9e9
Matthew B <106352182+artntek@users.noreply.github.com>
2025-07-30 10:28:18 -0700

    add helm-get-values-filter-by()

--- utils/k8s-utility-functions.sh
+++ utils/k8s-utility-functions.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 
-# This script contains utility functions for Kubernetes operations.
+## HELM-based utility functions for Kubernetes operations. You can source this file in your shell
+# session, or you can copy functions to your ~/.zshrc or ~/.bashrc for repeated access
 
-## List all images deployed on the cluster, across all namespaces, grouped by Helm release name
-## Optionally provide a command-line4 argument to highlight images whose descriptor matches a
-## specific string.
-## Examples:
-##   helm-list-images-filter-by "bitnami/"
-## ...will highlight:
-##    ⚠️   Image: docker.io/bitnami/pgbouncer:1.16.1 ⚠️
+
+## Get the values of a Helm release and filter by a specific YAML key. Using --all will return all
+## values that match the filter, not just user-overridden values.
+## Usage:
+##   helm-get-values-filter-by [release-name] [yaml-key] [--all]
+## Example:
+##   helm-get-values-filter-by metacatarctic "image" --all
 ##
-## ...but will not highlight:
+## This will return all values for the specified key across all releases that match the filter.
+##
+helm-get-values-filter-by() {
+  if [ -z "$1" ]; then
+    echo "usage: helm-get-values-filter-by [release-name] [yaml-key] [--all]"
+    echo "NOTE: make sure you're in the correct context and namespace"
+    return 1
+  fi
+  release=$1
+  filter_by=$2
+  expr=$(printf '.. | select(has("%s")) | {"path": path, "%s": .["%s"]}' "$filter_by" "$filter_by" "$filter_by")
+  helm get values "$release" $3 | yq eval "$expr" -
+}
+
+
+## #NOTE: MUST BE RUN AS ADMIN (dev-k8s or prod-k8s)
+## List all images deployed by all Helm releases, and if an `alert-match` string is provided,
+## highlight matching images in the output.
+## Usage:
+## $ helm-list-images-filter-by [alert-match]
+##
+## Example: to find any bitnami images that are not sourced form the bitnamilegacy repo:
+## $ helm-list-images-filter-by "bitnami/"
+## #...will highlight:
+##    ⚠️   Image: docker.io/bitnami/pgbouncer:1.16.1 ⚠️
+## #...but will not highlight:
 ##    🐳  Image: docker.io/bitnamilegacy/postgresql:17.5.0-debian-12-r20
 ##
 helm-list-images-filter-by() {
